b16904e46dd1e96089cc9bd0c25e0091ba1614c9
orthagonal <orthagonal1@gmail.com>
2018-08-28 20:53:14 +0000

    v1

--- example/example.js
+++ example/example.js
@@ -4,6 +4,7 @@ const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 const f = async () => {
   const server = new Hapi.Server({ port: 8080 });
   await server.register(require('../index.js'));
+  // declare a route that takes a random amount of time before returning:
   server.route({
     method: 'get',
     path: '/slow',
@@ -12,15 +13,20 @@ const f = async () => {
       return 'ok';
     }
   });
+  // declare a route that errors randomly:
   server.route({
     method: 'get',
     path: '/error',
     handler(request, h) {
-      throw new Error('an error!');
+      if (Math.random() > .5) {
+        throw new Error('an error!');
+      }
+      return 'whew!';
     }
   })
   await server.start();
   console.log('Server started on port 8080');
+  // get an array of calls to the /error and /slow routes (they are all Promises):
   const promisepromises = [];
   for (var i = 0; i < 50; i++) {
     promisepromises.push(server.inject({
@@ -32,11 +38,14 @@ const f = async () => {
       url: '/slow'
     }));
   }
+  // now wait for all those calls to return:
   await Promise.all(promisepromises);
+  // now get the raw metrics from hapi-prom:
   const metrics = await server.inject({
     url: '/metrics',
     method: 'get'
   });
+  console.log('Metrics are:');
   console.log(metrics.payload);
   await server.stop();
 };
