8db473f2ed8acea649803b3ca3ca7487ac57f311
orthagonal <orthagonal1@gmail.com>
2018-08-30 21:10:26 +0000

    some updates

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const collectDefaultMetrics = prom.collectDefaultMetrics;
 const defaults = {
   metricsPath: '/metrics',
   defaultMetrics: false,
+  defaultLabels: false,
   labels: {
-    duration: ['method', 'path', 'status'],
     buckets: ['method', 'path', 'status']
   }
 };
 
 const register = (server, pluginOptions) => {
   const options = Object.assign({}, defaults, pluginOptions);
+  if (options.defaultLabels) {
+    prom.register.setDefaultLabels(options.defaultLabels);
+  }
   if (options.defaultMetrics) {
     collectDefaultMetrics({ timeout: 5000 });
   }
@@ -19,8 +22,7 @@ const register = (server, pluginOptions) => {
   const metric = {
     http: {
       requests: {
-        duration: new prom.Summary({ name: 'http_request_duration_milliseconds', help: 'request duration in milliseconds', labelNames: options.labels.duration }),
-        buckets: new prom.Histogram({ name: 'http_request_buckets_milliseconds', help: 'request duration buckets in milliseconds. Bucket sizes set to .1, .3, 1.2, 5', labelNames: options.labels.buckets, buckets: [ .1, .3, 1.2, 5 ] })
+        buckets: new prom.Histogram({ name: 'http_request_duration_seconds', help: 'request duration buckets in seconds. Bucket sizes set to .1, .3, 1.2, 5', labelNames: options.labels.buckets, buckets: [ .1, .3, 1.2, 5 ] })
       }
     }
   };
@@ -43,7 +45,6 @@ const register = (server, pluginOptions) => {
     }
     const duration = ms(request.plugins['hapi-prom'].start) / 1000; // log in seconds
     // register the duration, broken down by method, path and HTTP code:
-    metric.http.requests.duration.labels(request.method, request.route.path, request.response.statusCode).observe(duration);
     metric.http.requests.buckets.labels(request.method, request.route.path, request.response.statusCode).observe(duration);
   });
   server.route({
